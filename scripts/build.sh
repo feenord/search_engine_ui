14e9677952bc6e9e59f5f7d9b9d858002a43efb3
feenord <feenord40@gmail.com>
2022-05-16 00:11:12 +0300

    изменение скрипта build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 yc compute image delete crawler-ui
-packer build -force -var-file=variables.json deploy_exe.json
-terraform apply -auto-approve
+packer build -force -var-file=variables.json -var "git_sha=`git rev-parse --short HEAD`" deploy_exe.json
+terraform apply -auto-approve -var "git_sha=`git rev-parse --short HEAD`"
